def88260bfb88d6b69a1aba30ed45d7c6ca7e575
Sepehr Safari <sdnsoft@gmail.com>
2018-08-17 14:49:04 -0400

    Prop

--- limit-service/src/main/java/com/marketplace/limit/PropConfigurations.java
+++ limit-service/src/main/java/com/marketplace/limit/PropConfigurations.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 
 @Component
-@ConfigurationProperties("limit-service")
+@ConfigurationProperties()
 public class PropConfigurations {
 	private int minimum;
 	private int maximum;
